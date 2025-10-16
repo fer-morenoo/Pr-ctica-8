ef028f3f4bba549bd49b2ad35055c4246144e0c8
fer-morenoo <fernanda.delen11@gmail.com>
2025-10-16 13:29:22 -0600

    Create programa1.c

---
+++ programa1.c
@@ -0,0 +1,14 @@
+/* Fernanda Arely Moreno De León
+práctica 8 ejercicio 1 */
+
+int main()
+{
+ int num, cont = 0;
+ printf("\a----- Tabla de multiplicar -----\n");
+ printf("Ingrese un número: \n");
+ scanf("%d", &num);
+ printf("La tabla de multiplicar del %d es:\n", num);
+ while (++cont <= 10)
+ printf("%d x %d = %d\n", num, cont, num*cont);
+ return 0;
+}

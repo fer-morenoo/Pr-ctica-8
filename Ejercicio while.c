d82b53745da6aef62d1671957c48c5a2128f724a
fer-morenoo <fernanda.delen11@gmail.com>
2025-11-07 19:32:44 -0600

    Create Ejercicio while.c

---
+++ Ejercicio while.c
@@ -0,0 +1,21 @@
+/* Fernanda Arely Moreno De León
+Ejercicio con WHILE */
+
+#include <stdio.h>
+void main()
+{
+    int n;
+    do
+    {
+        printf("Dame un numero: \n");
+        scanf("%d", &n);
+    }
+    while(n<=0);
+    int num=5, s=1;
+    while(s<=n)
+    {
+        printf("El valor para la serie es: %d \n", num);
+        num=num+5;
+        s=s+1;
+    }
+}
